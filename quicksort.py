1f9e282c86c87e9003c97fae850541f1deab781a
zachduffill <zachdach321@googlemail.com>
2024-11-25 18:50:53 +0000

    fixed bugs, finished quicksort.py

--- quicksort.py
+++ quicksort.py
@@ -1,43 +1,35 @@
-arr = []
-
 def qs(arr,lo,hi):
-    if (lo >= hi):
-        # print(f"lo:{lo}, hi:{hi}, arr:{arr}")
-        return arr
-    
-    arr,pivotIdx = partition(arr,lo,hi)
-    # print("lo: ",lo," hi: ",hi)
-    arr = qs(arr,lo,pivotIdx-1)
-    arr = qs(arr,pivotIdx+1,hi)
+    if (lo < hi): # partition array and sort
+        arr,pivotIdx = partition(arr,lo,hi)
+        arr = qs(arr,lo,pivotIdx-1)
+        arr = qs(arr,pivotIdx+1,hi)
 
     return arr
 
-def swap(arr,i1,i2):
+def swap(arr,i1,i2): # swap two array items
     tmp = arr[i1]
     arr[i1] = arr[i2]
     arr[i2] = tmp
     return arr
 
 def partition(arr,lo,hi):
+    # set pivot to middle value in subarray
     pivotIdx = lo + (hi-lo)//2
     pivot = arr[pivotIdx]
 
-    arr = swap(arr,pivotIdx,hi)
+    arr = swap(arr,pivotIdx,hi) # swap pivot with last value in subarray
 
-    i = lo-1
-
-    for j in range(lo, hi-1):
-        # print(j," ",arr)
+    # move all values less than pivot's value to the start of the subarray
+    i = lo-1 
+    for j in range(lo, hi): 
         if arr[j] <= pivot:
             i+=1
             arr = swap(arr,j,i)
 
-    arr = swap(arr,i+1,hi)
-    return arr,pivotIdx
+    arr = swap(arr,i+1,hi) # swap pivot to correct index, and return
+    return arr,i+1
 
-def quicksort(a):
-    arr = a
-    arr = qs(arr,0,len(arr)-1)
-    print(arr)
+def quicksort(arr):
+    return qs(arr,0,len(arr)-1)
 
-quicksort([5,7,3,6,9,22,4,12,0])
+print(quicksort([5,7,3,6,9,22,4,12,0]))
