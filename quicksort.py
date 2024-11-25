a09109e89839310fac339eda2c51428d02b5757e
zachduffill <zachdach321@googlemail.com>
2024-11-25 17:53:46 +0000

    implemented partition, swap, and qs; currently does not work

--- quicksort.py
+++ quicksort.py
@@ -1,9 +1,41 @@
+from math import floor
+
+arr = []
+
 def qs(arr,lo,hi):
     if (lo >= hi):
-        pass
+        return
+    
+    pivotIdx = partition(arr,lo,hi)
+    qs(arr,lo,pivotIdx-1)
+    qs(arr,pivotIdx+1,hi)
+    
+
+def swap(arr,i1,i2):
+    tmp = arr[i1]
+    arr[i1] = arr[i2]
+    arr[i2] = tmp
+    return arr
 
 def partition(arr,lo,hi):
-    pass
+    print(hi)
+    pivotIdx = len(arr)//2
+    pivot = arr[pivotIdx]
 
-def quicksort(arr):
+    arr = swap(arr,pivotIdx,hi)
+
+    i = lo-1
+
+    for j in range(lo, hi-1):
+        # print(j," ",arr)
+        if arr[j] <= pivot:
+            i+=1
+            arr = swap(arr,j,i)
+
+    return pivotIdx
+
+def quicksort(a):
+    arr = a
     qs(arr,0,len(arr)-1)
+
+quicksort([5,7,3,6,9,22,4,12,0])
